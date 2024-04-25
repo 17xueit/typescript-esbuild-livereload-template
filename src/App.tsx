b60dc6015845dc8fbe7f3a72fb7f17e080f24c30
yangwawa0323 <yangwawa0323@163.com>
2024-04-25 14:00:09 +0800

    remove the livereload , just use live-server no-css-inject

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
 import { StrictMode } from 'react';
 
 export const App = () => {
-    return (
-        <StrictMode>
-            <img src="./img/react.svg" className='react-logo' />
-            <h1>Hello from React &lt;App&gt;</h1>
-            <span>Edit src/App.tsx</span>
-        </StrictMode>
-    );
+	return (
+		<StrictMode>
+			<p>This serve is run parallism!</p>
+			<img
+				src='./img/react.svg'
+				className='react-logo'
+			/>
+			<h1>Hello from React &lt;App&gt;</h1>
+			<span>Edit src/App.tsx</span>
+			<input
+				className='bg-blue-400 rounded-sm shadow-sm px-6 py-2 placeholder:text-gray-500'
+				placeholder='Input your name!'
+			/>
+		</StrictMode>
+	);
 };
